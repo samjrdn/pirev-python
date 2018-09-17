8f65aac2af5ee18fe16cdf6093ad54863a3c2863
Sam Jordan <sam@jrdn.ca>
2018-09-17 12:39:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pirev",
-    version="1.0.0",
+    version="1.0.1",
     author="Sam Jordan",
     author_email="sam@jrdn.ca",
     description="Raspberry Pi revision utility",
