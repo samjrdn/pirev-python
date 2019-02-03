09771b81db4bcd6148973a44d447aba21f1d7429
Sam Jordan <sam.jordan@shopify.com>
2019-02-03 13:38:54 -0500

    version 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pirev",
-    version="1.1.0",
+    version="1.2.0",
     author="Sam Jordan",
     author_email="sam@jrdn.ca",
     description="Raspberry Pi revision utility",
