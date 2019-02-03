a735c28ef0484a1012518d3ce837065e5019b754
Sam Jordan <sam.jordan@shopify.com>
2019-02-03 13:38:20 -0500

    Add tests for code tables

--- test/test.py
+++ test/test.py
@@ -1,16 +1,28 @@
 import unittest
+import csv
 from pirev.pirev import getInfo
+from pirev.revinfo import getRevInfo
 
 def testDataPath(filename):
   return f'test/data/{filename}'
+
+def readTsvData(filename):
+  with open(filename) as tsvfile:
+    reader = csv.DictReader(tsvfile, dialect='excel-tab')
+    data = []
+    for row in reader:
+      data.append(dict((k.lower(), v) for k,v in row.items()))
+    return data
 
 CPUINFO_PI1 = testDataPath('cpuinfo_pi1')
 CPUINFO_PI3 = testDataPath('cpuinfo_pi3')
 CPUINFO_MBP = testDataPath('cpuinfo_mbp')
 MISSING_FILE = testDataPath('missing')
+OLD_CODES = testDataPath('codes_old.tsv')
+NEW_CODES = testDataPath('codes_new.tsv')
 
 class TestGetInfo(unittest.TestCase):
-  
+
   def test_cpuinfo_pi1(self):
     info = getInfo(CPUINFO_PI1)
 
@@ -22,7 +34,7 @@ class TestGetInfo(unittest.TestCase):
       'manufacturer': 'Sony UK',
       'code': '000e',
     })
-  
+
   def test_cpuinfo_pi3(self):
     info = getInfo(CPUINFO_PI3)
 
@@ -34,14 +46,38 @@ class TestGetInfo(unittest.TestCase):
       'manufacturer': 'Embest',
       'code': 'a22082',
     })
-  
+
   def test_cpuinfo_mbp(self):
     with self.assertRaises(ReferenceError):
       getInfo(CPUINFO_MBP)
-  
+
   def test_missing_file(self):
     with self.assertRaises(FileNotFoundError):
       getInfo(MISSING_FILE)
+
+class TestRevInfo(unittest.TestCase):
+
+  def test_parse_old_codes(self):
+    data = readTsvData(OLD_CODES)
+
+    for entry in data:
+      info = getRevInfo(entry['code'])
+
+      self.assertEqual(info['type'], entry['model'])
+      self.assertEqual(info['memory'], entry['ram'])
+      self.assertEqual(info['revision'], float(entry['revision']))
+      self.assertEqual(info['manufacturer'], entry['manufacturer'])
+
+  def test_parse_new_codes(self):
+    data = readTsvData(NEW_CODES)
+
+    for entry in data:
+      info = getRevInfo(entry['code'])
+
+      self.assertEqual(info['type'], entry['model'])
+      self.assertEqual(info['memory'], entry['ram'])
+      self.assertEqual(info['revision'], float(entry['revision']))
+      self.assertEqual(info['manufacturer'], entry['manufacturer'])
 
 if __name__ == '__main__':
     unittest.main()
